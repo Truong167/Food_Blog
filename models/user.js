220e9c842909ff4bfb8485a3f286cd08755e0541
truong711 <vietruong7111@gmail.com>
2023-02-21 09:34:25 +0700

    Fix register

--- models/user.js
+++ models/user.js
@@ -27,7 +27,6 @@ module.exports = (sequelize, DataTypes) => {
   }
   User.init({
     userId: {
-      // allowNull: false,
       primaryKey: true,
       autoIncrement: true,
       type: DataTypes.INTEGER,
