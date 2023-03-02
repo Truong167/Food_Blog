41440c9fc08a436a0a8c2cae02c9b615eefdf5cb
truong711 <vietruong7111@gmail.com>
2023-03-02 21:56:42 +0700

    Update format date

--- middlewares/utils/formatDate.js
+++ middlewares/utils/formatDate.js
@@ -1,7 +1,7 @@
 const moment = require('moment')
 
 const formatDate = function(date) {
-    return moment(date).format('DD-MM-YYYY HH:mm:ss');
+    return moment(date).utcOffset("+07:00").format('DD-MM-YYYY HH:mm:ss');
 }
 
 
