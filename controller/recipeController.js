e71e9825b5fa3787b939e31585f05ed9da5fdfe1
truong711 <vietruong7111@gmail.com>
2023-04-09 10:17:34 +0700

    Update

--- controller/recipeController.js
+++ controller/recipeController.js
@@ -459,7 +459,7 @@ class recipeController {
             } catch (error) {
                 res.status(500).json({
                     success: false, 
-                    message: error,
+                    message: error.message,
                     data: ""
                 })
             }
